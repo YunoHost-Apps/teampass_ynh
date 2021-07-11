48e4b08a63ebf773648c32d59bfb5c22c792c39c
ericgaspar <junk.eg@free.fr>
2021-07-11 13:33:44 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -207,19 +207,6 @@ ynh_system_reload () {
 
 #=================================================
 
-# Delete a file checksum from the app settings
-#
-# $app should be defined when calling this helper
-#
-# usage: ynh_remove_file_checksum file
-# | arg: file - The file for which the checksum will be deleted
-ynh_delete_file_checksum () {
-	local checksum_setting_name=checksum_${1//[\/ ]/_}	# Replace all '/' and ' ' by '_'
-	ynh_app_setting_delete $app $checksum_setting_name
-}
-
-#=================================================
-
 ynh_maintenance_mode_ON () {
 	# Load value of $path_url and $domain from the config if their not set
 	if [ -z $path_url ]; then
