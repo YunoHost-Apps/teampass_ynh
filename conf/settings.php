4a94d8099cf68cc35ac04011550b97ca27e2c688
ericgaspar <junk.eg@free.fr>
2021-07-11 16:53:01 +0200

    Update settings.php

--- conf/settings.php
+++ conf/settings.php
@@ -4,9 +4,9 @@ global $server, $user, $pass, $database, $pre, $db, $port, $encoding;
 
 ### DATABASE connexion parameters ###
 $server = "localhost";
-$user = "__DB_USER__";
+$user = "__DB_NAME__";
 $pass = "__DB_PWD__";
-$database = "__DB_USER__";
+$database = "__DB_NAME__";
 $pre = "teampass_";
 $port = 3306;
 $encoding = "utf8";
