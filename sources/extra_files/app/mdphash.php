ab60ebe0b0412c384aa2e4bcc50e0be80d4e7fd0
yalh76 <yalh@yahoo.com>
2022-07-24 15:39:53 +0200

    Fixes

--- sources/extra_files/app/mdphash.php
+++ sources/extra_files/app/mdphash.php
@@ -1,16 +1,24 @@
 <?php
 //function bCrypt tirée directement de sources/main.functions.php, ligne 223.
+/**
+ * genHash().
+ *
+ * Generate a hash for user login
+ *
+ * @param string $password
+ */
 function bCrypt($password, $cost)
 {
     $salt = sprintf('$2y$%02d$', $cost);
     if (function_exists('openssl_random_pseudo_bytes')) {
         $salt .= bin2hex(openssl_random_pseudo_bytes(11));
     } else {
-        $chars='./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for ($i=0; $i<22; $i++) {
-            $salt.=$chars[mt_rand(0, 63)];
+        $chars = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for ($i = 0; $i < 22; ++$i) {
+            $salt .= $chars[mt_rand(0, 63)];
         }
     }
+
     return crypt($password, $salt);
 }
 
