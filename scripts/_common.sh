43b9d20a93b27db59159ee0654b945739f19cd70
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-23 21:58:32 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="7.4"
-
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-bcmath php$YNH_PHP_VERSION-iconv php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-mysqli"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
